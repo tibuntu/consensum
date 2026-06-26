2707315a30ebcdcd0359d4c84f71fb235cb6f7cc
Timo Hankamer <timo.hankamer@tibuntu.dev>
2026-06-26 07:24:56 +0200

    chore(main): release 0.9.0

--- lib/version.ts
+++ lib/version.ts
@@ -1,2 +1,2 @@
 // This file's version is maintained by release-please. Do not edit by hand.
-export const APP_VERSION = "0.8.0"; // x-release-please-version
+export const APP_VERSION = "0.9.0"; // x-release-please-version
